ce940e279a8eaad5040bfc6fc65f56a250e5c848
doanHa <dmha@uh.edu>
2018-10-18 00:36:48 -0500

    10/18/2018 at 12:33pm
    Added new animations for Coin, QBlock, Flower, Big Mario, Star.
    Added a new Player called Big_mario, with two children of GroundCheck and CeilingCheck
    Added new scripts for Big_Mario Crouching, Power Up(Incomplete)
    Added a new Tag for Power Up

---
+++ MarioProject/Assets/Scripts/PlayerCrouch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCrouch : MonoBehaviour {
+
+    public LayerMask ceiling;
+    public Transform ceilingCheck;
+    public float ceilingCheckRad;
+    public bool touchedCeiling;
+    public bool isCrouching;
+
+    private void Update()
+    {
+        gameObject.GetComponent<Animator>().SetBool("isCrouching", isCrouching);
+        gameObject.GetComponent<Animator>().SetBool("touchCeiling", touchedCeiling);
+
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            isCrouching = true;
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+            gameObject.GetComponent<CircleCollider2D>().enabled = false;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            gameObject.GetComponent<CapsuleCollider2D>().enabled = true;
+            gameObject.transform.Find("GroundCheck").transform.localPosition = new Vector3(0, -0.1148f,0);
+            gameObject.transform.Find("CeilingCheck").transform.localPosition = new Vector3(0.007f, 0.105f, 0);
+        }
+        else
+        {
+            isCrouching = false;
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+            gameObject.GetComponent<CircleCollider2D>().enabled = true;
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+            gameObject.transform.Find("GroundCheck").transform.localPosition = new Vector3(0, -0.154f, 0);
+            gameObject.transform.Find("CeilingCheck").transform.localPosition = new Vector3(0.007f, 0.154f, 0);
+        }
+        
+    }
+    void FixedUpdate()
+    {
+        touchedCeiling = Physics2D.OverlapCircle(ceilingCheck.position, ceilingCheckRad, ceiling);
+    }
+}
---
+++ MarioProject/Assets/Scripts/Power_up.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power_up : MonoBehaviour {
+
+    // Use this for initialization
+    public int power_type;
+    public GameObject normal_mario;
+    public GameObject big_mario;
+    public Sprite fire_power;
+    public Sprite invincible_power;
+    public float power_up_timer;
+	void Start () {
+        power_type = 0;
+        power_up_timer = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Power Up")
+        {
+
+            if(col.gameObject.name.Contains("Red Mushroom"))
+            {
+                power_type = 1;
+            }
+            if (col.gameObject.name.Contains("Flower"))
+            {
+                power_type = 2;
+            }
+            if(col.gameObject.name.Contains("Star"))
+            {
+                power_type += 3;
+            }
+            Destroy(col.gameObject);
+            getPower();
+        }
+        if (col.gameObject.tag == "Enemy")
+        {
+            power_type = 0;
+            Destroy(col.gameObject);
+            getPower();
+        }
+    }
+    private void getPower()
+    {
+        if(power_type == 0)
+        {
+            normal_mario.transform.position = big_mario.transform.position;
+            big_mario.SetActive(false);
+            normal_mario.SetActive(true);
+        }
+        if(power_type == 1)
+        {
+            big_mario.transform.position = normal_mario.transform.position;
+            big_mario.SetActive(true);
+            normal_mario.SetActive(false);
+        }
+        if(power_type == 2)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = fire_power;
+        }
+        if(power_type == 3)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = invincible_power;
+            this.gameObject.GetComponent<Animator>().runtimeAnimatorController = 
+                Resources.Load("small Mario invincible") as RuntimeAnimatorController;
+            while(power_up_timer > 0.0)
+            {
+                power_up_timer -= Time.deltaTime;
+            }
+            if (power_up_timer == 0)
+            {
+                power_type -= 3;
+                getPower();
+            }
+        }
+    }
+}
