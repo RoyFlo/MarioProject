329ac6427cda700f264f199a656900ee037ccc52
doanHa <dmha@uh.edu>
2018-11-05 14:31:20 -0600

    Fixed a few logic part scripts
    Fixed a few logic in QBlock Script
    Fix a few logic in Star Movement Script
    Added Star Movement script 2
    Changed a few code in CollectiblesMoveUp Script.

--- MarioProject/Assets/Scripts/CollectiblesMoveUp.cs
+++ MarioProject/Assets/Scripts/CollectiblesMoveUp.cs
@@ -6,15 +6,30 @@ public class CollectiblesMoveUp : MonoBehaviour {
     private Vector3 newPosition;
     private void Start()
     {
-        newPosition = new Vector3(transform.position.x, transform.position.y + 0.16f, 0);
+        newPosition = new Vector3(transform.position.x, transform.position.y + 0.164f, 0);
     }
     void Update () {
-        transform.position = Vector3.MoveTowards(transform.position, newPosition, 0.07f * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, 0.08f * Time.deltaTime);
         if(transform.position == newPosition)
         {
-            gameObject.GetComponent<MushroomMovement>().enabled = true;
-            gameObject.GetComponent<Rigidbody2D>().gravityScale = 3;
-            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            if (gameObject.name.Contains("Mushroom"))
+            {
+                gameObject.GetComponent<MushroomMovement>().enabled = true;
+            }
+            if(gameObject.name.Contains("Star"))
+            {
+                gameObject.GetComponent<StarMovement>().enabled = true;
+            }
+            if(gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                gameObject.GetComponent<Rigidbody2D>().gravityScale = 3;
+            }
+            if(gameObject.GetComponent<BoxCollider2D>() != null)
+            {
+                gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            }
+            //           gameObject.transform.SetParent(null);
+            gameObject.GetComponent<CircleCollider2D>().enabled = true;
             gameObject.GetComponent<CollectiblesMoveUp>().enabled = false;
         }
     }
--- MarioProject/Assets/Scripts/MushroomMovement.cs
+++ MarioProject/Assets/Scripts/MushroomMovement.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class MushroomMovement : MonoBehaviour {
     public float mushroom_move_direction;
     public float mushroom_move_speed;
-	// Update is called once per frame
+
 	void Update () {
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(mushroom_move_direction, 0) * mushroom_move_speed;
     }
+    // change direction only when collided with pipe
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Pipe")
