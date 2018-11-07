f435435526e6da72c439e06be59906375931b440
Lindsay Ortiz <Lindsay.ortiz@live.com>
2018-11-06 19:36:13 -0600

    no message

--- MarioProject/Assets/Scripts/SoundFXScript.cs
+++ MarioProject/Assets/Scripts/SoundFXScript.cs
@@ -2,17 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SoundFX {
+    JUMP,
+    BRICK_BREAK,
+    COLLECT_COIN,
+    COLLECT_MUSHROOM,
+    COLLECT_STAR,
+    COLLECT_1UP
+}
+
 public class SoundFXScript : MonoBehaviour {
 
     public AudioSource jumpSource;
     public AudioSource destroyBrickSource;
     public AudioSource coinSource;
+
+    PlayerMove2 playerMove;
+
+    /*
+     * Needed sound effects:
+     * Big mushroom
+     * Star
+     * 1-up
+     */
+
+   void Start() {
+
+    }
+
 	
 	void Update () {
+
 		if(Input.GetKeyDown(KeyCode.Space)) {
             jumpSource.Play();
         }
 	}
+
+    public void playFX(SoundFX soundFX) {
+        switch (soundFX) {
+            case SoundFX.JUMP:
+                jumpSource.Play();
+                break;
+            case SoundFX.BRICK_BREAK:
+                destroyBrickSource.Play();
+                break;
+            case SoundFX.COLLECT_COIN:
+                coinSource.Play();
+                break;
+            case SoundFX.COLLECT_MUSHROOM:
+            case SoundFX.COLLECT_STAR:
+            case SoundFX.COLLECT_1UP:
+                break;
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D trig) {
         if (trig.gameObject.tag == "DestroyBrick") {
