4ab21a26cd0999789aa6cac921e02dcfaeba214e
RoyFlo <43392885+RoyFlo@users.noreply.github.com>
2018-11-12 12:48:20 -0600

    warped

---
+++ MarioProject/Assets/Scripts/PipeWarp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PipeWarp : MonoBehaviour {
+
+    public int load;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                Application.LoadLevel(load);
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
