b877ba578d5f5b885b9db890e52503d03f73be9a
RoyFlo <43392885+RoyFlo@users.noreply.github.com>
2018-11-12 15:31:16 -0600

    warp complete

--- MarioProject/Assets/Scripts/PipeWarp.cs
+++ MarioProject/Assets/Scripts/PipeWarp.cs
@@ -14,7 +14,7 @@ public class PipeWarp : MonoBehaviour {
 
     }
 	
-    void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerStay2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player")
         {
