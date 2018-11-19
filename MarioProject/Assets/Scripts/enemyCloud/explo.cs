9138d5d0346f3f45789ce8bed280b0f10e5b1ddc
JackyC0919 <jacky226633@gmail.com>
2018-11-19 15:06:59 -0600

    bomb explosion animation
    +enemy kill mario

---
+++ MarioProject/Assets/Scripts/enemyCloud/explo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explo : MonoBehaviour {
+
+    private float exploTime = 1.0f;
+    private float timeCounter = 0.0f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeCounter += Time.deltaTime;
+        if (timeCounter > exploTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
