05381f952f536bdecd9f50383b338edfdfdd19b3
AllanKey <allankey4@gmail.com>
2018-11-20 10:22:09 -0600

    Updating prefabs for Koopa and bulletBill

---
+++ MarioProject/Assets/Scripts/BulletBill.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBill : MonoBehaviour {
+
+    // Use this for initialization
+    Rigidbody2D myBody;
+    Transform myTrans;
+    public float speed = 1;
+    public GameObject bullet;
+
+    void Start () {
+        myTrans = this.transform;
+        myBody = this.GetComponent<Rigidbody2D>();
+        bullet = GameObject.FindGameObjectWithTag("Bullet");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 myVelocity = myBody.velocity;
+        myVelocity.x = myTrans.right.x * speed;
+        myBody.velocity = myVelocity;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(bullet);
+        }
+    }
+}
