d45912ff6dc40a06c73fa25ed53803ef985a452f
AllanKey <allankey4@gmail.com>
2018-11-25 22:51:13 -0600

    Adding spawner

--- MarioProject/Assets/Scripts/BulletBill.cs
+++ MarioProject/Assets/Scripts/BulletBill.cs
@@ -11,20 +11,24 @@ public class BulletBill : MonoBehaviour {
     public GameObject bullet;
     public GameObject player;
     public Death_respawn m_someOtherScriptOnAnotherGameObject;
+    public Rigidbody2D bulletPrefab;
 
     void Start () {
         myTrans = this.transform;
         myBody = this.GetComponent<Rigidbody2D>();
         bullet = GameObject.FindGameObjectWithTag("Bullet");
         player = GameObject.FindGameObjectWithTag("Player");
+
+     //   for (int i = 0; i < 20; i++)
+     //   {
+     //       ExecuteAfterTime(1);
+     //   }
     }
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
         Vector2 myVelocity = myBody.velocity;
         myVelocity.x = myTrans.right.x * speed;
         myBody.velocity = myVelocity;
-
     }
 
     private void OnTriggerEnter2D(Collider2D col)
---
+++ MarioProject/Assets/Scripts/TSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TSpawn : MonoBehaviour {
+
+    public float spawnTime;        // The amount of time between each spawn.
+    public float spawnDelay;       // The amount of time before spawning starts.
+    public GameObject bulletBill;
+    public Vector3 originalPos;
+    public GameObject bulletBlaster;
+
+    void Start()
+    {   
+        bulletBlaster = GameObject.FindGameObjectWithTag("Bullet Blaster");
+        originalPos = bulletBlaster.transform.position;
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+    public void SpawnObject()
+    {
+        transform.eulerAngles = new Vector3(0, -180, 0);
+        Instantiate(bulletBill, originalPos, transform.rotation);
+    }
+}
+
