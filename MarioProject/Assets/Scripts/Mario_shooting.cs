a91c7ec24fcbeb77e224c5079b09e5f7b2742e63
doanHa <dmha@uh.edu>
2018-11-25 21:30:54 -0600

    Update
    Minor change to Mario shooting script

--- MarioProject/Assets/Scripts/Mario_shooting.cs
+++ MarioProject/Assets/Scripts/Mario_shooting.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Mario_shooting : MonoBehaviour {
     public Transform firePoint;
     public GameObject fireBullet;
+    public float numberOfBullet;
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            if(GameObject.FindGameObjectsWithTag("Bullet").Length < 3)
-            shootBullet();
+            if(GameObject.FindGameObjectsWithTag("Bullet").Length < numberOfBullet)
+                shootBullet();
         }
 	}
     public void shootBullet()
