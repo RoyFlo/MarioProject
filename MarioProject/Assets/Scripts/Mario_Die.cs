7ecea9e5be840a28e0be6432844dfd8add5dac86
JackyC0919 <jacky226633@gmail.com>
2018-11-26 16:29:46 -0600

    add mario_Die

---
+++ MarioProject/Assets/Scripts/Mario_Die.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mario_Die : MonoBehaviour {
+
+    private float TimeCounter = 0.0f;
+    private float aniTime = 1.0f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TimeCounter += Time.deltaTime;
+        if (TimeCounter > aniTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
