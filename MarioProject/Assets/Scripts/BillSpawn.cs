c897ea4a1c36cc2c5bd493585412192067c59bb6
RoyFlo <43392885+RoyFlo@users.noreply.github.com>
2018-11-26 17:38:26 -0600

    added bullet bill

---
+++ MarioProject/Assets/Scripts/BillSpawn.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillSpawn : MonoBehaviour {
+    public GameObject bulletBill;
+    public Transform launcher;
+
+    public float LocateTime = 0;
+    public float LocateNeedTime = 3;
+    public float ShootingTime = 1.0f;
+    public float ShootingNeedTime = 1.0f;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AITime();
+        SpawnObject();
+    }
+
+    void AITime()
+    {
+        if (LocateTime <= 0)  //if LocateTime <= 0
+        {
+            LocateTime = LocateNeedTime;  //set LocateTime to LocateNeedTime
+        }
+        else //
+        {
+            LocateTime -= Time.deltaTime;
+        }
+    }
+
+    public void SpawnObject()
+    {
+        if (ShootingTime <= 0)  //ShootingTime <= 0
+        {
+            ShootingTime = ShootingNeedTime;
+            Instantiate(bulletBill, launcher.position, Quaternion.identity);
+        }
+        else
+        {
+            ShootingTime -= Time.deltaTime;
+        }
+        
+    }
+
+    
+}
---
+++ MarioProject/Assets/Scripts/TSpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TSpawn : MonoBehaviour {
+
+    public float spawnTime;        // The amount of time between each spawn.
+    public float spawnDelay;       // The amount of time before spawning starts.
+    public GameObject bulletBill;
+    public Vector3 originalPos;
+    public GameObject bulletBlaster;
+    public int bulletCount;
+
+    void Start()
+    {
+        bulletCount = 0;
+        bulletBlaster = GameObject.FindGameObjectWithTag("Bullet Blaster");
+        originalPos = bulletBlaster.transform.position;
+        while (bulletCount < 5)
+        {
+            InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+        }
+    }
+    public void SpawnObject()
+    {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            Instantiate(bulletBill, originalPos, transform.rotation);
+            bulletCount++;
+    }
+}
+
