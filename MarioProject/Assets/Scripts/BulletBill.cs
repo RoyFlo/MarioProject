ba4dedf74acd7a00e5a927dfe18f11be60e86734
Lindsay Ortiz <Lindsay.ortiz@live.com>
2018-11-26 19:08:07 -0600

    Download master - no need to merge

--- MarioProject/Assets/Scripts/BulletBill.cs
+++ MarioProject/Assets/Scripts/BulletBill.cs
@@ -11,34 +11,39 @@ public class BulletBill : MonoBehaviour {
     public GameObject bullet;
     public GameObject player;
     public Death_respawn m_someOtherScriptOnAnotherGameObject;
+    public Rigidbody2D bulletPrefab;
 
     void Start () {
         myTrans = this.transform;
         myBody = this.GetComponent<Rigidbody2D>();
         bullet = GameObject.FindGameObjectWithTag("Bullet");
         player = GameObject.FindGameObjectWithTag("Player");
+
+     //   for (int i = 0; i < 20; i++)
+     //   {
+     //       ExecuteAfterTime(1);
+     //   }
     }
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
         Vector2 myVelocity = myBody.velocity;
         myVelocity.x = myTrans.right.x * speed;
         myBody.velocity = myVelocity;
-
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag == "Player")
+        if (col.tag == "Player")
         {
-            Destroy(bullet);
+            Destroy(gameObject);
         }
     }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.GetType() == typeof(CircleCollider2D))
-        {
-            m_someOtherScriptOnAnotherGameObject.DIE();
-        }
-    }
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.collider.GetType() == typeof(CircleCollider2D) && collision.gameObject.tag == "Player")
+    //    {
+    //        Destroy(player);
+    //        //m_someOtherScriptOnAnotherGameObject.DIE();
+    //    }
+    //}
 }
